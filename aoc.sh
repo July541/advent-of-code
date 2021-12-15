90afcdd51072e240ee9d2b90a9d5d912146f46f0
Lei Zhu <julytreee@gmail.com>
2021-12-15 23:55:25 +0800

    more efficiency day 6

--- aoc.sh
+++ aoc.sh
@@ -4,7 +4,7 @@ if [ "$#" != "2" ]
 then
   echo "Usage: ./aoc.sh <year> <day>"
   echo "Support year: 2021"
-  echo "Support day: 1-4"
+  echo "Support day: 1-30"
 fi
 
 RUST_MIN_STACK=8388608 cargo test --lib aoc$1_day$2 -- --nocapture
--- src/aoc2021/day6/solution.rs
+++ src/aoc2021/day6/solution.rs
@@ -1,44 +1,46 @@
-fn read_fishes() -> Vec<i128> {
+use std::collections::HashMap;
+
+fn read_fishes() -> Vec<i32> {
   let relative_path = "/src/aoc2021/day6/input";
   let path = crate::utils::input_path(relative_path);
 
   let contents = std::fs::read_to_string(path).unwrap();
   let fishes = contents
     .split(",")
-    .map(|x| x.parse::<i128>().unwrap())
-    .collect::<Vec<i128>>();
+    .map(|x| x.parse::<i32>().unwrap())
+    .collect::<Vec<i32>>();
 
   fishes
 }
 
-fn count_fish(fishes: &mut Vec<i128>, day: i32) -> usize {
-  const NEW_FISH_DAY: i128 = 8;
+fn count_fish(fishes: &Vec<i32>, day: i32) -> u128 {
+  let mut mp:HashMap<i32, u128> = std::collections::HashMap::new();
+
+  fishes.iter().for_each(|x| {
+    *mp.entry(*x).or_insert(0) += 1;
+  });
   
-  fn one_day(fishes: &mut Vec<i128>) {
-    fishes.iter_mut().for_each(|x| *x -= 1);
-    let new_fish_cnt = fishes.iter().filter(|x| **x == -1).count();
-    fishes.iter_mut().for_each(|x| *x = if *x < 0 { 6 } else { *x });
-    let mut new_fishs = vec![NEW_FISH_DAY; new_fish_cnt];
-    fishes.append(&mut new_fishs);
-  }
-
   for _ in 0..day {
-    one_day(fishes);
+    let mut cur = HashMap::new();
+    for (k, v) in mp {
+      if k == 0 {
+        *cur.entry(6).or_insert(0) += v;
+        *cur.entry(8).or_insert(0) += v;
+      } else {
+        *cur.entry(k - 1).or_insert(0) += v;
+      }
+    }
+    mp = cur;
   }
 
-  fishes.len()
-}
-
-fn count_fish_256(fishes: &Vec<i128>) -> i128 {
-  let v = (0..7).map(|x| count_fish(&mut vec![x], 256) as i128).collect::<Vec<_>>();
-  fishes.iter().map(|x| v[*x as usize]).sum()
+  mp.into_iter().map(|x| x.1).sum()
 }
 
 pub fn lantern_fish() {
-  let fishes = &mut read_fishes();
+  let fishes = read_fishes();
 
-  let res1 = count_fish(fishes, 80);
-  let res2 = count_fish_256(fishes);
+  let res1 = count_fish(&fishes, 80);
+  let res2 = count_fish(&fishes, 256);
   println!("{} lanternfish would be there after 80 days.", res1);
   println!("{} lanternfish would be there after 256 days.", res2);
 }
